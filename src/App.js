c52bc78a34ecd7635722895943557d4bbc8cf6fe
Tejashree Salvi <trajendra@altimetrik.com>
2024-02-28 13:43:08 +0530

    migration-accelerator-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import migrationImage from './migrateb.svg';
+import { AppBar, Toolbar, Typography, Divider } from '@mui/material';
+import MigrationForm from './containers/tools/bitbucket';
+import { Grid, Paper } from '@mui/material';
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" >
+            Migration Accelerator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Grid container spacing={2}>
+      {/* Left section with the form */}
+        <Grid item xs={12} sm={6}>
+          <MigrationForm/>
+        </Grid>
+        <Divider orientation="horizontal" />
+      {/* Right section with waves */}
+        <Grid item xs={6} sm={6} style={{ position: 'fixed', top: '64px', right: 0, bottom: 0, zIndex: 0 }}>
+            <img src={migrationImage} style={{maxWidth: '70%', maxHeight: '80%', padding: "150px", paddingTop: "100px" }} />
+        </Grid>
+    </Grid>
     </div>
   );
 }
---
+++ src/containers/tools/bitbucket/index.js
@@ -0,0 +1,255 @@
+import React, { useState, useEffect } from "react";
+import * as Mui from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { migrateScript } from "../../../api/accelerator";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container
+} from "@mui/material";
+
+const initialFormData = {
+  selectedTool: "",
+  clientHost: "",
+  clientUsername: "",
+  clientPassword: "",
+  serverHost: "",
+  serverUsername: "",
+  serverPassword: "",
+  workspace: "",
+  serviceName: "",
+};
+
+const categories = [
+  { value: 'scm_migration', label: 'SCM Migration', options: ['Bitbucket', 'Github', 'GitLab', 'SVN'] },
+  { value: 'artifactory_migration', label: 'Artifactory Migration', options: ['Jfrog Artifactory', 'Nexus'] },
+  { value: 'pipeline_migration', label: 'Pipeline Migration', options: ['Jenkins'] },
+];
+
+const theme = createTheme({
+  // Customize your theme here (e.g., colors, typography)
+  palette: {
+    primary: { main: "#007bff" }, // Adjust primary color
+    secondary: { main: "#ffc107" }, // Adjust secondary color
+  },
+  typography: {
+    h5: {
+      fontSize: "1.5rem", // Adjust heading size
+    },
+  },
+});
+
+const ToolForm = () => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+
+  const [selectedCategory, setSelectedCategory] = useState('');
+  // const [selectedOption, setSelectedOption] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleToolChange = (event) => {
+    setFormData({
+      ...formData,
+      clientHost: "",
+      clientUsername: "",
+      clientPassword: "",
+      serverHost: "",
+      serverUsername: "",
+      serverPassword: "",
+      // Keep workspace and serviceName (if needed)
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Make a POST request to your API endpoint
+      const response = await migrateScript(formData);
+      // Handle the API response
+      console.log("API Response:", response.data);
+
+      // Reset the form fields
+      setFormData(initialFormData);
+    } catch (error) {
+      // Handle API call error
+      console.error("Error:", error);
+    }
+  };
+
+  const checkFormValidity = () => {
+    for (const key in formData) {
+      if (formData[key] === '') {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // code for category....
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+    // setSelectedOption(''); // Reset option on category change
+  };
+
+  // const handleOptionChange = (event) => {
+  //   setSelectedOption(event.target.value);
+  // };
+
+  const selectedCategoryOptions =
+    categories.find((category) => category.value === selectedCategory)?.options || [];
+
+
+
+
+  useEffect(() => {
+    setIsFormValid(checkFormValidity());
+  }, [formData]);
+
+  // ... your API call logic ...
+
+  return (
+    <Container maxWidth="sm" justifyContent="center">
+      <Grid item xs={12} sm={12} padding={4}>
+        <Typography variant="h5" sx={{ mb: 2, color: '#1976D2' }}>
+          Tool Migration
+        </Typography>
+
+
+        <form onSubmit={handleSubmit}>
+
+          <FormControl fullWidth margin="normal">
+            <Select
+              labelId="category-label"
+              id="category"
+              value={selectedCategory}
+              label="Select Category"
+              onChange={handleCategoryChange}
+              name="selectedCategory"
+              sx={{ mb: 2 }}
+            >
+              {categories.map((category) => (
+                <MenuItem key={category.value} value={category.value}>
+                  {category.label}
+                </MenuItem>
+              ))}
+            </Select>
+
+            {selectedCategory && (
+              <Select
+                labelId="category-label"
+                value={formData.tool}
+                label="Select tool"
+                onChange={handleInputChange}
+                name="tool"
+                disabled={!selectedCategory} // Disable option dropdown if no category selected
+                sx={{ mb: 2 }}
+                aria-label="Select the migration category" 
+              >
+                {selectedCategoryOptions.map((option) => (
+                  <MenuItem key={option} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </Select>
+            )}
+
+            {/* actual code */}
+
+          </FormControl>
+          {formData.tool === "Bitbucket" && (
+            <div>
+              <TextField
+                fullWidth
+                label="Client Host"
+                name="clientHost"
+                value={formData.clientHost}
+                onChange={handleInputChange}
+                placeholder="e.g., https://git.altimetrik.com/bitbucket"
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Client Username"
+                name="clientUsername"
+                value={formData.clientUsername}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Client Password"
+                type="password"
+                name="clientPassword"
+                value={formData.clientPassword}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Server Host"
+                name="serverHost"
+                value={formData.serverHost}
+                placeholder="e.g., https://bitbucket.org/"
+                onChange={handleInputChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Server Username"
+                name="serverUsername"
+                value={formData.serverUsername}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Server Password"
+                type="password"
+                name="serverPassword"
+                value={formData.serverPassword}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                label="Workspace"
+                name="workspace"
+                value={formData.workspace}
+                onChange={handleInputChange}
+                margin="normal"
+              />
+            </div>
+          )}
+
+          {formData.tool === "Bitbucket" && (
+            <TextField
+              fullWidth
+              label="Service Name"
+              name="serviceName"
+              value={formData.serviceName}
+              onChange={handleInputChange}
+              placeholder="e.g., BB-FULL-SC-VERSION"
+              margin="normal"
+            />
+          )}
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Migrate
+          </Button>
+        </form>
+      </Grid>
+    </Container>
+  );
+};
+
+export default ToolForm;
